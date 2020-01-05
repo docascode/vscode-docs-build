4ccd718c9ac70a0cb06d482ac01788bfdad8cf3a
Jiayin Pei <jipe@microsoft.com>
2020-01-05 22:30:48 +0800

    Report unit test coverage

---
+++ tasks/testTasks.ts
@@ -0,0 +1,34 @@
+import * as gulp from 'gulp';
+import spawnNode from './spawnNode';
+import { nycPath, unitTestCoverageRootPath, mochaPath } from './projectPaths';
+
+gulp.task('test:e2e', async () => {
+    // TODO: add e2e test
+});
+
+gulp.task('test:component', async () => {
+    // TODO: add component test
+});
+
+gulp.task('test:unit', async () => {
+    return spawnNode([
+        nycPath,
+        '-r',
+        'lcovonly',
+        '--report-dir',
+        unitTestCoverageRootPath,
+        mochaPath,
+        '--require',
+        'ts-node/register',
+        '--ui',
+        'bdd',
+        '--',
+        'test/unitTests/**/*.test.ts'
+    ]);
+});
+
+gulp.task('test', gulp.series(
+    'test:e2e',
+    'test:component',
+    'test:unit'
+));
