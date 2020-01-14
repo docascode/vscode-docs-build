493e7ae44e08b6dfd4f175b3d211a788afa5fd1e
JiayinPei <jipe@microsoft.com>
2020-01-14 16:25:34 +0800

    address comments

--- src/observers/buildStatusBarObserver.ts
+++ src/observers/buildStatusBarObserver.ts
@@ -1,15 +1,15 @@
-import { BaseStatusBarItemObserver } from "./BaseStatusBarObserver";
+import { BaseStatusBarObserver } from "./BaseStatusBarObserver";
 import { EventType } from "../common/EventType";
 import { BaseEvent } from "../common/loggingEvents";
 
-export class BuildStatusBarObserver extends BaseStatusBarItemObserver {
+export class BuildStatusBarObserver extends BaseStatusBarObserver {
     public eventHandler = (event: BaseEvent) => {
         switch (event.type) {
             case EventType.BuildInstantAllocated:
-                this.SetAndShowStatusBar(`$(sync~spin)`, undefined, undefined, 'Building the current workspace folder');
+                this.setAndShowStatusBar(`$(sync~spin)`, undefined, undefined, 'Building the current workspace folder');
                 break;
             case EventType.BuildInstantReleased:
-                this.SetAndShowStatusBar(`$(sync)`, undefined, undefined, 'Ready to Build');
+                this.setAndShowStatusBar(`$(sync)`, undefined, undefined, 'Ready to Build');
                 break;
         }
     }
