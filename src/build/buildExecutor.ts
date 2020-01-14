ff921203f9b1ba395979ef9c94674f9e76bed8ef
JiayinPei <jipe@microsoft.com>
2020-01-14 13:57:12 +0800

    improve build performance

--- src/build/buildExecutor.ts
+++ src/build/buildExecutor.ts
@@ -3,15 +3,17 @@ import { PACKAGE_JSON, EXTENSION_PATH, extensionConfig } from '../shared';
 import { PlatformInformation } from '../common/PlatformInformation';
 import { ChildProcess } from 'child_process';
 import { Package, AbsolutePathPackage } from '../dependency/Package';
-import { DocfxRestoreCanceled, DocfxRestoreFailed, DocfxRestoreSucceeded, DocfxBuildCanceled, DocfxBuildSucceeded, DocfxBuildFailed } from '../common/loggingEvents';
+import { DocfxRestoreCanceled, DocfxRestoreFailed, DocfxRestoreSucceeded, DocfxBuildCanceled, DocfxBuildSucceeded, DocfxBuildFailed, DocfxBuildStarted, DocfxRestoreStarted } from '../common/loggingEvents';
 import { EnvironmentController } from '../common/EnvironmentController';
 import { EventStream } from '../common/EventStream';
 import { executeDocfx } from '../utils/childProcessUtils';
+import { basicAuth } from '../utils/utils';
 
 export class BuildExecutor {
     private cwd: string;
     private binary: string;
     private runningChildProcess: ChildProcess;
+    private skipRestore: boolean;
 
     constructor(platformInfo: PlatformInformation, private environmentController: EnvironmentController, private eventStream: EventStream) {
         let runtimeDependencies = <Package[]>PACKAGE_JSON.runtimeDependencies;
@@ -19,6 +21,7 @@ export class BuildExecutor {
         let absolutePackage = AbsolutePathPackage.getAbsolutePathPackage(buildPackage, EXTENSION_PATH);
         this.cwd = absolutePackage.installPath.value;
         this.binary = absolutePackage.binary;
+        this.skipRestore = false;
     }
 
     public async RunBuild(
@@ -37,9 +40,11 @@ export class BuildExecutor {
             'DOCS_ENVIRONMENT': this.environmentController.env
         };
 
-        // TODO: Restore on VS Code restore instead of every build
-        if (!(await this.restore(repositoryPath, outputPath, buildUserToken, envs))) {
-            return false;
+        if (!this.skipRestore) {
+            if (!(await this.restore(repositoryPath, outputPath, buildUserToken, envs))) {
+                return false;
+            }
+            this.skipRestore = true;
         }
 
         return await this.build(repositoryPath, outputPath, envs);
@@ -57,15 +62,24 @@ export class BuildExecutor {
         buildUserToken: string,
         envs: any): Promise<boolean> {
         return new Promise((resolve, reject) => {
-            let stdinInput = JSON.stringify({
-                "http": {
-                    [`${extensionConfig.OPBuildAPIEndPoint[this.environmentController.env]}`]: {
-                        "headers": {
-                            "X-OP-BuildUserToken": buildUserToken
-                        }
+            let secrets = <any>{
+                [`${extensionConfig.OPBuildAPIEndPoint[this.environmentController.env]}`]: {
+                    "headers": {
+                        "X-OP-BuildUserToken": buildUserToken
                     }
                 }
+            };
+            if (process.env.VSCODE_DOCS_BUILD_EXTENSION_GITHUB_TOKEN) {
+                secrets["https://github.com"] = {
+                    "headers": {
+                        "authorization": `basic ${basicAuth(process.env.VSCODE_DOCS_BUILD_EXTENSION_GITHUB_TOKEN)}`
+                    }
+                };
+            }
+            let stdinInput = JSON.stringify({
+                "http": secrets
             });
+            this.eventStream.post(new DocfxRestoreStarted());
             let command = `${this.binary} restore "${repositoryPath}" --legacy --output ${outputPath} --stdin`;
             this.runningChildProcess = executeDocfx(
                 command,
@@ -94,7 +108,8 @@ export class BuildExecutor {
         outputPath: string,
         envs: any): Promise<boolean> {
         return new Promise((resolve, reject) => {
-            let command = `${this.binary} build "${repositoryPath}" --legacy --output ${outputPath}`;
+            this.eventStream.post(new DocfxBuildStarted());
+            let command = `${this.binary} build "${repositoryPath}" --legacy --dry-run --output ${outputPath}`;
             this.runningChildProcess = executeDocfx(
                 command,
                 this.eventStream,
