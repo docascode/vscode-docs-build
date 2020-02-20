ec330e0684a0b58cdf978b6932997bbb3c46f592
Jiayin Pei <jipe@microsoft.com>
2020-02-20 10:06:06 +0800

    use Nodejs assert instead of chai since it cannot handle error equality

--- test/componentTests/index.ts
+++ test/componentTests/index.ts
@@ -1,9 +1,6 @@
-import { use } from 'chai';
 import * as path from 'path';
 import * as Mocha from 'mocha';
 import * as glob from 'glob';
-
-use(require('chai-subset'));
 
 export async function run(): Promise<void> {
     const nyc = process.env.COVERAGE ? setupCoverage() : undefined;
