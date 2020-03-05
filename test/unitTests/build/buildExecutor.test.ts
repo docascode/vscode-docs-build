ce8ed876e3f4b770e1cc733f908193db6ab2d62d
Jiayin Pei <jipe@microsoft.com>
2020-03-05 11:46:34 +0800

    add UT for BuildExecutor (#83)
    * add UT for BuildExecutor
    
    * fix

---
+++ test/unitTests/build/buildExecutor.test.ts
@@ -0,0 +1,304 @@
+import cp from 'child_process';
+import assert from 'assert';
+import path from 'path';
+import * as childProcessUtil from '../../../src/utils/childProcessUtils';
+import * as utils from '../../../src/utils/utils';
+import { EventStream } from '../../../src/common/eventStream';
+import { EnvironmentController } from '../../../src/common/environmentController';
+import { SinonSandbox, createSandbox, SinonStub } from 'sinon';
+import TestEventBus from '../../utils/testEventBus';
+import { getFakeEnvironmentController, getFakedTelemetryReporter, fakedExtensionContext, fakedBuildInput, testFolder, setTelemetryUserOptInToFalse, setTelemetryUserOptInToTrue, getFakedWindowsPlatformInformation, getFakedNonWindowsPlatformInformation } from '../../utils/faker';
+import { PlatformInformation } from '../../../src/common/platformInformation';
+import TelemetryReporter from '../../../src/telemetryReporter';
+import { BuildExecutor } from '../../../src/build/buildExecutor';
+import { DocfxRestoreStarted, DocfxRestoreCompleted, BuildCacheSizeCalculated, DocfxBuildStarted, DocfxBuildCompleted } from '../../../src/common/loggingEvents';
+import { DocfxExecutionResult, BuildResult } from '../../../src/build/buildResult';
+import { setTimeout } from 'timers';
+import { EventType } from '../../../src/common/eventType';
+import { Subscription } from 'rxjs';
+
+describe('BuildExecutor', () => {
+    let sinon: SinonSandbox;
+    let stubExecuteDocfx: SinonStub;
+
+    let eventStream: EventStream;
+    let subscription: Subscription;
+    let fakedEnvironmentController: EnvironmentController;
+    let fakedWindowsPlatformInformation: PlatformInformation;
+    let fakedNonWindowsPlatformInformation: PlatformInformation;
+    let fakedTelemetryReporter: TelemetryReporter;
+    let buildExecutor: BuildExecutor;
+    let testEventBus: TestEventBus;
+
+    let executedCommands: string[];
+    let executedOptions: cp.ExecOptions[];
+    let executedStdinInput: string[];
+    let killProcessTreeFuncCalled: boolean;
+
+    before(() => {
+        eventStream = new EventStream();
+        fakedEnvironmentController = getFakeEnvironmentController();
+        fakedWindowsPlatformInformation = getFakedWindowsPlatformInformation();
+        fakedNonWindowsPlatformInformation = getFakedNonWindowsPlatformInformation();
+        fakedTelemetryReporter = getFakedTelemetryReporter();
+        buildExecutor = new BuildExecutor(fakedExtensionContext, fakedWindowsPlatformInformation, fakedEnvironmentController, eventStream, fakedTelemetryReporter);
+
+        testEventBus = new TestEventBus(eventStream);
+
+        sinon = createSandbox();
+        stubExecuteDocfx = undefined;
+
+        sinon.stub(utils, 'getFolderSizeInMB').resolves(10);
+        sinon.stub(utils, 'killProcessTree').callsFake((pid: number, signal: string) => {
+            killProcessTreeFuncCalled = true;
+            return Promise.resolve();
+        });
+    });
+
+    function mockExecuteDocfx(restoreExitCode: number, buildExitCode: number) {
+        stubExecuteDocfx && stubExecuteDocfx.restore();
+        stubExecuteDocfx = sinon.stub(childProcessUtil, 'executeDocfx').callsFake(
+            function (command: string,
+                eventStream: EventStream,
+                exitHandler: (code: number, signal: string) => void,
+                options?: cp.ExecOptions,
+                stdinInput?: string): cp.ChildProcess {
+
+                let childKilled = false;
+                executedCommands.push(command);
+                executedOptions.push(options);
+                executedStdinInput.push(stdinInput);
+                if (command.indexOf(' restore ') !== -1) {
+                    setTimeout(() => {
+                        if (!childKilled) {
+                            exitHandler(restoreExitCode, undefined);
+                        }
+                    }, 10);
+                } else if (command.indexOf(' build ') !== -1) {
+                    setTimeout(() => {
+                        if (!childKilled) {
+                            exitHandler(buildExitCode, undefined);
+                        }
+                    }, 10);
+                }
+
+                return <any>{
+                    pid: 1234,
+                    kill: (signal: string) => {
+                        childKilled = true;
+                        exitHandler(0, signal);
+                    }
+                };
+            });
+    }
+
+    beforeEach(() => {
+        executedCommands = [];
+        executedOptions = [];
+        executedStdinInput = [];
+        testEventBus.clear();
+    });
+
+    afterEach(() => {
+    });
+
+    after(() => {
+        sinon.restore();
+    });
+
+    describe('Restore', () => {
+        it('Restore failed', async () => {
+            mockExecuteDocfx(1, 0);
+
+            let buildResult = await buildExecutor.RunBuild('fakedCorrelationId', fakedBuildInput, 'faked-build-token');
+
+            assert.deepStrictEqual(testEventBus.getEvents(), [
+                new DocfxRestoreStarted(),
+                new DocfxRestoreCompleted(DocfxExecutionResult.Failed, 1),
+                new BuildCacheSizeCalculated('fakedCorrelationId', 10),
+            ]);
+            assert.equal(buildResult.result, DocfxExecutionResult.Failed);
+            assert.equal(buildResult.isRestoreSkipped, false);
+
+            // If restore does not succeeded, it should not be skipped in the next build
+            testEventBus.clear();
+            await buildExecutor.RunBuild('fakedCorrelationId', fakedBuildInput, 'faked-build-token');
+
+            assert.deepStrictEqual(testEventBus.getEvents(), [
+                new DocfxRestoreStarted(),
+                new DocfxRestoreCompleted(DocfxExecutionResult.Failed, 1),
+                new BuildCacheSizeCalculated('fakedCorrelationId', 10),
+            ]);
+        });
+
+        it('Restore Cancelled', (done) => {
+            mockExecuteDocfx(0, 0);
+            killProcessTreeFuncCalled = false;
+
+            let buildPromise = buildExecutor.RunBuild('fakedCorrelationId', fakedBuildInput, 'faked-build-token');
+            setTimeout(async () => {
+                await buildExecutor.cancelBuild();
+                let buildResult = await buildPromise;
+                assert.deepStrictEqual(testEventBus.getEvents(), [
+                    new DocfxRestoreStarted(),
+                    new DocfxRestoreCompleted(DocfxExecutionResult.Canceled, 0),
+                    new BuildCacheSizeCalculated('fakedCorrelationId', 10),
+                ]);
+                assert.equal(buildResult.result, DocfxExecutionResult.Canceled);
+                assert.equal(buildResult.isRestoreSkipped, false);
+                assert.equal(killProcessTreeFuncCalled, true);
+                done();
+            }, 5);
+        });
+    });
+
+    describe('Build', () => {
+        before(() => {
+            mockExecuteDocfx(0, 0);
+        });
+
+        beforeEach(() => {
+            testEventBus.clear();
+        });
+
+        afterEach(() => {
+            subscription && subscription.unsubscribe();
+        });
+
+        it('First Time to run build successfully', async () => {
+            let buildResult = await buildExecutor.RunBuild('fakedCorrelationId', fakedBuildInput, 'faked-build-token');
+
+            assert.equal(buildResult.result, DocfxExecutionResult.Succeeded);
+            assert.equal(buildResult.isRestoreSkipped, false);
+
+            assert.deepStrictEqual(executedCommands, [
+                `docfx.exe restore "${path.resolve(testFolder, 'fakedRepositoyPath')}" --legacy --output "${path.resolve(testFolder, 'fakedRepositoyPath', '_site')}" --stdin`,
+                `docfx.exe build "${path.resolve(testFolder, 'fakedRepositoyPath')}" --legacy --dry-run --output "${path.resolve(testFolder, 'fakedRepositoyPath', '_site')}"`,
+            ]);
+            assert.deepStrictEqual(executedOptions, [
+                {
+                    cwd: `${path.resolve(testFolder, 'fakedExtensionPath', '.docfx')}`,
+                    env: {
+                        APPINSIGHTS_INSTRUMENTATIONKEY: '4424c909-fdd9-4229-aecb-ad2a52b039e6',
+                        DOCFX_CORRELATION_ID: 'fakedCorrelationId',
+                        DOCFX_REPOSITORY_URL: 'https://faked.original.repository',
+                        DOCS_ENVIRONMENT: 'PROD'
+                    }
+                },
+                {
+                    cwd: `${path.resolve(testFolder, 'fakedExtensionPath', '.docfx')}`,
+                    env: {
+                        APPINSIGHTS_INSTRUMENTATIONKEY: '4424c909-fdd9-4229-aecb-ad2a52b039e6',
+                        DOCFX_CORRELATION_ID: 'fakedCorrelationId',
+                        DOCFX_REPOSITORY_URL: 'https://faked.original.repository',
+                        DOCS_ENVIRONMENT: 'PROD'
+                    }
+                }
+            ]);
+            assert.deepStrictEqual(executedStdinInput, [
+                JSON.stringify({
+                    "http": {
+                        "https://op-build-prod.azurewebsites.net": {
+                            "headers": {
+                                "X-OP-BuildUserToken": "faked-build-token"
+                            }
+                        }
+                    }
+                }),
+                undefined
+            ]);
+            assert.deepStrictEqual(testEventBus.getEvents(), [
+                new DocfxRestoreStarted(),
+                new DocfxRestoreCompleted(DocfxExecutionResult.Succeeded, 0),
+                new BuildCacheSizeCalculated('fakedCorrelationId', 10),
+                new DocfxBuildStarted(),
+                new DocfxBuildCompleted(DocfxExecutionResult.Succeeded, 0)
+            ]);
+        });
+
+        it('Second time to trigger build successfully', async () => {
+            let buildResult = await buildExecutor.RunBuild('fakedCorrelationId', fakedBuildInput, 'faked-build-token');
+
+            assert.equal(buildResult.result, DocfxExecutionResult.Succeeded);
+            assert.equal(buildResult.isRestoreSkipped, true);
+            assert.deepStrictEqual(testEventBus.getEvents(), [
+                new DocfxBuildStarted(),
+                new DocfxBuildCompleted(DocfxExecutionResult.Succeeded, 0)
+            ]);
+        });
+
+        it('Build Failed', async () => {
+            mockExecuteDocfx(0, 1);
+
+            let buildResult = await buildExecutor.RunBuild('fakedCorrelationId', fakedBuildInput, 'faked-build-token');
+
+            assert.equal(buildResult.result, DocfxExecutionResult.Failed);
+            assert.equal(buildResult.isRestoreSkipped, true);
+            assert.deepStrictEqual(testEventBus.getEvents(), [
+                new DocfxBuildStarted(),
+                new DocfxBuildCompleted(DocfxExecutionResult.Failed, 1)
+            ]);
+        });
+
+        it('Build Cancelled', (done) => {
+            mockExecuteDocfx(0, 0);
+            killProcessTreeFuncCalled = false;
+            let buildPromise: Promise<BuildResult>;
+
+            subscription = eventStream.subscribe((event) => {
+                if (event.type === EventType.DocfxBuildStarted) {
+                    setTimeout(async () => {
+                        let buildResult: BuildResult;
+                        try {
+                            await buildExecutor.cancelBuild();
+                            buildResult = await buildPromise;
+                        } catch (err) {
+                            console.log(err);
+                        }
+                        assert.deepStrictEqual(testEventBus.getEvents(), [
+                            new DocfxBuildStarted(),
+                            new DocfxBuildCompleted(DocfxExecutionResult.Canceled, 0)
+                        ]);
+                        assert.equal(buildResult.result, DocfxExecutionResult.Canceled);
+                        assert.equal(buildResult.isRestoreSkipped, true);
+                        assert.equal(killProcessTreeFuncCalled, true);
+                        done();
+                    }, 5);
+                }
+            });
+
+            buildPromise = buildExecutor.RunBuild('fakedCorrelationId', fakedBuildInput, 'faked-build-token');
+        });
+
+        it('Telemetry disabled', async () => {
+            setTelemetryUserOptInToFalse(fakedTelemetryReporter);
+
+            await buildExecutor.RunBuild('fakedCorrelationId', fakedBuildInput, 'faked-build-token');
+
+            assert.deepStrictEqual(executedOptions, [
+                {
+                    cwd: `${path.resolve(testFolder, 'fakedExtensionPath', '.docfx')}`,
+                    env: {
+                        DOCFX_CORRELATION_ID: 'fakedCorrelationId',
+                        DOCFX_REPOSITORY_URL: 'https://faked.original.repository',
+                        DOCS_ENVIRONMENT: 'PROD'
+                    }
+                }
+            ]);
+            setTelemetryUserOptInToTrue(fakedTelemetryReporter);
+        });
+
+        it('Non-Windows Platform', async () => {
+            buildExecutor = new BuildExecutor(fakedExtensionContext, fakedNonWindowsPlatformInformation, fakedEnvironmentController, eventStream, fakedTelemetryReporter);
+
+            await buildExecutor.RunBuild('fakedCorrelationId', fakedBuildInput, 'faked-build-token');
+
+            assert.deepStrictEqual(executedCommands, [
+                `./docfx build "${path.resolve(testFolder, 'fakedRepositoyPath')}" --legacy --dry-run --output "${path.resolve(testFolder, 'fakedRepositoyPath', '_site')}"`,
+            ]);
+
+            // Reset environment
+            buildExecutor = new BuildExecutor(fakedExtensionContext, fakedWindowsPlatformInformation, fakedEnvironmentController, eventStream, fakedTelemetryReporter);
+        });
+    });
+});
--- test/utils/faker.ts
+++ test/utils/faker.ts
@@ -1,9 +1,14 @@
+import path from 'path';
 import { EnvironmentController } from '../../src/common/environmentController';
 import { AbsolutePathPackage } from '../../src/dependency/package';
 import { SinonSandbox } from 'sinon';
 import { KeyChain } from '../../src/credential/keyChain';
 import { UserInfo, DocsRepoType } from '../../src/shared';
 import { Credential } from '../../src/credential/credentialController';
+import { ExtensionContext } from '../../src/extensionContext';
+import { PlatformInformation } from '../../src/common/platformInformation';
+import TelemetryReporter from '../../src/telemetryReporter';
+import { BuildInput, BuildType } from '../../src/build/buildInput';
 
 export function getFakeEnvironmentController(docsRepoType: DocsRepoType = 'GitHub'): EnvironmentController {
     return {
@@ -18,6 +23,34 @@ export function setEnvToPROD(environmentController: EnvironmentController) {
 
 export function setEnvToPPE(environmentController: EnvironmentController) {
     environmentController.env = 'PPE';
+}
+
+export function getFakedWindowsPlatformInformation(): PlatformInformation {
+    return <PlatformInformation>{
+        isWindows: () => true,
+        rid: 'win7-x64'
+    };
+}
+
+export function getFakedNonWindowsPlatformInformation(): PlatformInformation {
+    return <PlatformInformation>{
+        isWindows: () => false,
+        rid: 'osx-x64'
+    };
+}
+
+export function getFakedTelemetryReporter(): TelemetryReporter {
+    return <TelemetryReporter>{
+        getUserOptIn: () => true
+    };
+}
+
+export function setTelemetryUserOptInToTrue(telemetryReporter: TelemetryReporter){
+    telemetryReporter.getUserOptIn = ()=> true;
+}
+
+export function setTelemetryUserOptInToFalse(telemetryReporter: TelemetryReporter){
+    telemetryReporter.getUserOptIn = ()=> false;
 }
 
 export function setupKeyChain(sinon: SinonSandbox, keyChain: KeyChain, userInfo: UserInfo) {
@@ -43,4 +76,36 @@ export const fakedCredential = <Credential>{
         userName: 'Faked User',
         userToken: 'faked-token'
     }
+};
+
+export const testFolder = path.resolve(__dirname, '.temp');
+
+export const fakedExtensionContext = <ExtensionContext>{
+    packageJson: {
+        runtimeDependencies: [
+            {
+                id: "docfx-win7-x64",
+                name: "docfx",
+                binary: "docfx.exe",
+                installPath: ".docfx",
+                rid: "win7-x64",
+            },
+            {
+                id: "docfx-osx-x64",
+                name: "docfx",
+                binary: "./docfx",
+                installPath: ".docfx",
+                rid: "osx-x64",
+            }
+        ]
+    },
+    extensionPath: path.resolve(testFolder, 'fakedExtensionPath'),
+    extensionVersion: '0.0.1'
+};
+
+export const fakedBuildInput = <BuildInput>{
+    buildType: BuildType.FullBuild,
+    localRepositoryPath: path.resolve(testFolder, 'fakedRepositoyPath'),
+    localRepositoryUrl: 'https://faked.repository',
+    originalRepositoryUrl: 'https://faked.original.repository'
 };
