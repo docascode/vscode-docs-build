1c6e02a4fbc15ab594f3e80216a6ae212f604325
JiayinPei <jipe@microsoft.com>
2020-09-28 10:41:39 +0800

    update

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Docs Validation
 
-This extension has been republished with new publisher `docsmsft`, please move to the new extension [docsmsft.docs-build](vscode:extension/docsmsft.docs-build) and uninstall this one, Sorry for the inconvenience.
+This extension has been republished with new publisher `docsmsft` at [docsmsft.docs-build](vscode:extension/docsmsft.docs-build). Please uninstall this extension and install the new one. The future features and improvements will be published only in the new extension. Thanks for your understanding.
--- src/extension.ts
+++ src/extension.ts
@@ -3,7 +3,7 @@ import ExtensionExports from './common/extensionExport';
 
 export async function activate(context: vscode.ExtensionContext): Promise<ExtensionExports> {
     let input = await vscode.window.showInformationMessage(
-        "The extension `Docs validation(ceapex)` has been republished with new publisher `docsmsft`, please move to the new extension and uninstall this one",
+        "The extension `Docs validation(ceapex)` has been republished with new publisher `docsmsft`. Please uninstall this extension and install the new one. The future features and improvements will be published only in the new extension. Thanks for your understanding.",
         <MessageItem>{
             title: "Go to install the new extension"
         }
